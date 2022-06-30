78e30e0ec13e209b03758c2c58ea94c0fb7d66ff
jmcarriles <jmcarriles@eolos.la>
2022-06-30 20:52:10 -0300

    cambiando los alert a console.log

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ if (datoIngresado <= 10) {
 
     for (let i = 1; i <= datoIngresado; i++) {
 
-        alert('Este es el mensaje ' + i)
+        console.log('Este es el mensaje ' + i)
 
     }
 }
 else {
-    alert('solo se contar hasta 10 :(')
+    console.log('solo se contar hasta 10 :(')
 }
 
-alert('Muchas gracias!')
+console.log('Muchas gracias!')
